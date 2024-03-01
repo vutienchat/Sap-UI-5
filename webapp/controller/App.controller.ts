9166ba0eb1784b8dc9c0ca7190899af2268c73d2
chat vu <vuchat307@gmail.com>
2024-03-01 18:12:13 +0700

    v2

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,24 +1,6 @@
-import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
-import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
- * @name ui5.walkthrough.controller.App
+ * @namespace ui5.walkthrough.controller
  */
-export default class AppController extends Controller {
-  onShowHello(): void {
-    // read msg from i18n model
-    const recipient = (<JSONModel>this.getView()?.getModel())?.getProperty(
-      "/recipient/name"
-    );
-    const resourceBundle = <ResourceBundle>(
-      (<ResourceModel>this.getView()?.getModel("i18n"))?.getResourceBundle()
-    );
-    const msg =
-      resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
-    // show message
-    MessageToast.show(msg);
-  }
-}
+export default class App extends Controller {}
